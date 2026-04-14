5871deef4055a094c771792a35a9c1617d5627b3
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2026-04-14 01:04:53 +0000

    criacao da projeto em django e models da api_doadores

---
+++ doacao_sangue/api_doadores/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiDoadoresConfig(AppConfig):
+    name = "api_doadores"
---
+++ doacao_sangue/api_pacientes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiPacientesConfig(AppConfig):
+    name = "api_pacientes"
