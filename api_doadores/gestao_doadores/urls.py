ae2de1e01c6310069a44b279081fce2ad4ae0ad2
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2026-04-23 02:06:31 +0000

    feat: criaçao de views e definiçao de rotas

---
+++ api_doadores/gestao_doadores/urls.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework import routers 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView\
+from drf_yasg.views import get_schema_view 
+from drf_yasg import openapi 
+from rest_framework import permissions 
+
+from gestao_doadores.views import DoadorViewSet,CriterioDoacaoViewSet,LocalDoacaoViewSet,DemonstracaoInteresseViewSet
+
+router = routers.DefaultRouter() 
+router.register(r'doadores', DoadorViewSet, basename='doador')
+router.register(r'criterios',CriterioDoacaoViewSet, basename = 'criteriodoacao')
+router.register(r'locais',LocalDoacaoViewSet, basename = 'localdoacao')
+router.register(r'demonstracoes',DemonstracaoInteresseViewSet, basename = 'demonstracaointeresse')
+
+schema_view = get_schema_view( 
+    openapi.Info( 
+        title="API Doacao Sangue - Doadores", 
+        default_version='v1', 
+        description="Documentação da API de Doação de Sangue", 
+        contact=openapi.Contact(email="contato@exemplo.com"), 
+        license=openapi.License(name="MIT"), 
+    ), 
+    public=True, 
+    permission_classes=(permissions.AllowAny,), 
+)
+
+
+urlpatterns = [
+    path('', include(router.urls)),    
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Rotas da Documentação Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
--- api_doadores/gestao_doadores/views.py
+++ api_doadores/gestao_doadores/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions 
 
-# Create your views here.
+from .models import Doador,CriterioDoacao,LocalDoacao,DemonstracaoInteresse
+from .serializers import DoadorSerializer,CriterioDoacaoSerializer,LocalDoacaoSerializer,DemonstracaoInteresseSerializer
+
+class DoadorViewSet(viewsets.ModelViewSet):
+    queryset = Doador.objects.all() 
+    serializer_class = DoadorSerializer 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
+
+class CriterioDoacaoViewSet(viewsets.ModelViewSet):
+    queryset = CriterioDoacao.objects.all() 
+    serializer_class = CriterioDoacaoSerializer 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class LocalDoacaoViewSet(viewsets.ModelViewSet):
+    queryset = LocalDoacao.objects.all() 
+    serializer_class = LocalDoacaoSerializer 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class DemonstracaoInteresseViewSet(viewsets.ModelViewSet):
+    queryset = DemonstracaoInteresse.objects.all() 
+    serializer_class = DemonstracaoInteresseSerializer 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
